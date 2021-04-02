93c5aea06e8ec444796d4a03725e5240e4517751
srikarsashank <srikarsashank03@gmail.com>
2021-04-02 23:13:23 +0530

    heroku changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use(cors());
 app.use('/professionals',professionals)
 app.use('/app',routeURL)
 // Serve static assets under production
-if(process.env.NODE_ENV === 'production') {
+//if(process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req,res) => {
       res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
-}
+//}
 app.listen(PORT,console.log(`Server open at  ${PORT}`));
