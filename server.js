1e98e9a9c6faaaa9642eac5224196311e984aa92
srikarsashank <srikarsashank03@gmail.com>
2021-04-02 22:27:41 +0530

    changes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const routeURL = require('./Routes/route')
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
 const professionals = require('./Routes/professionals');
 const PORT=process.env.PORT || 4000;
 let corsOptions = {
@@ -19,7 +18,6 @@ dotenv.config()
 mongoose.connect(process.env.ACCESS_NIKHIL, () => console.log('database connected') )
 
 app.use(express.json())
-app.use(cookieParser())
 app.use(cors());
 app.use('/professionals',professionals)
 app.use('/app',routeURL)
